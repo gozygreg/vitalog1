5b955dde380f1fcf0222eafd10a100bb45709620
gozygreg <goziennabuife@yahoo.com>
2024-03-21 15:44:15 +0000

    add contact page and email functionality

--- vitalog/home/views.py
+++ vitalog/home/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 def index(request):
     return render(request, 'home/index.html', {})
+
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        send_mail(
+            'Welcome to Vitalog:' + message_name, #email subject
+            message,
+            message_email, # from or sender's email
+            ['community.vitalog@gmail.com'], # email patient is sending to
+            fail_silently=False,
+        )
+
+        messages.success(
+            request,
+            "Thank you, Your email sent successfully! We will get back to you shortly",
+        )
+
+        return redirect("index")
+
+    else: 
+        #return page
+        return render(request, 'home/contact.html', {})
--- vitalog/vitalog/settings.py
+++ vitalog/vitalog/settings.py
@@ -128,3 +128,16 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "vitalog@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
