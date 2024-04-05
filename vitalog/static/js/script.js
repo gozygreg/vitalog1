70dda7b48f32f10695c75f6fa2bc72a70c10de81
gozygreg <goziennabuife@yahoo.com>
2024-04-05 16:23:01 +0000

    update faq page

--- vitalog/static/js/script.js
+++ vitalog/static/js/script.js
@@ -1,23 +1,34 @@
 function toggleText(id) {
     var elem = document.getElementById(id);
-    var paragraphs = elem.getElementsByTagName("p");
-    var moreLink = elem.getElementsByClassName("more")[0];
+    var moreLink = elem.querySelector(".more");
+    var additionalContent = elem.querySelector(".additional-content");
 
-    // Check if the additional paragraphs are currently hidden
-    if (paragraphs[1].style.display === "none") {
-        // Show additional paragraphs
-        for (var i = 1; i < paragraphs.length; i++) {
-            paragraphs[i].style.display = 'block';
-        }
+    // Check if the additional content is currently hidden
+    if (additionalContent.style.display === "none" || additionalContent.style.display === "") {
+        // Show additional content
+        additionalContent.style.display = 'block';
         // Update the link text
         moreLink.textContent = "Read less";
     } else {
-        // Hide additional paragraphs
-        for (var i = 1; i < paragraphs.length; i++) {
-            paragraphs[i].style.display = 'none';
-        }
+        // Hide additional content
+        additionalContent.style.display = 'none';
         // Update the link text
         moreLink.textContent = "Read more";
     }
 }
 
+// Function to limit the number of words displayed initially
+function limitWords(element, limit) {
+    var text = element.textContent.trim(); // Trim to remove leading/trailing whitespace
+    var words = text.split(' ');
+    if (words.length > limit) {
+        var truncatedText = words.slice(0, limit).join(' ') + '...';
+        element.innerHTML = truncatedText + ' <span class="more" onclick="toggleText(this.parentElement.parentElement.id)">Read more</span>';
+    }
+}
+
+// Limit the words for each FAQ answer
+var faqElements = document.querySelectorAll('.text-myBlack p');
+faqElements.forEach(function (element) {
+    limitWords(element, 25); // Change 25 to desired word limit
+});
