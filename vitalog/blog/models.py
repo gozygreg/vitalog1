acd5d11e49e45e8890d616716ad50c5a08a08a15
gozygreg <goziennabuife@yahoo.com>
2024-04-05 15:48:31 +0000

    add cloudinary image

--- vitalog/blog/models.py
+++ vitalog/blog/models.py
@@ -14,10 +14,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    image = CloudinaryField(
-        "image",
-        default="placeholder",
-    )
+    image = CloudinaryField('image')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
