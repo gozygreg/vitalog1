11e1501f28e33b7a7be274377b48698277408b9f
gozygreg <goziennabuife@yahoo.com>
2024-04-09 13:20:42 +0000

    adjust index page frontend

--- vitalog/static/js/script.js
+++ vitalog/static/js/script.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toggles = document.querySelectorAll('.toggleText');
-    toggles.forEach(toggle => {
-        const moreBtn = toggle.querySelector('.more');
-        moreBtn.addEventListener('click', function () {
-            const additionalContent = toggle.querySelector('.additional-content');
-            if (additionalContent.style.display === "none" || additionalContent.style.display === "") {
-                additionalContent.style.display = 'block';
-                moreBtn.textContent = "Read less";
-            } else {
-                additionalContent.style.display = 'none';
-                moreBtn.textContent = "Read more";
-            }
+    if (window.innerWidth <= 768) { // Check if screen width is less than or equal to 768px (adjust as needed)
+        const toggles = document.querySelectorAll('.toggleText');
+        toggles.forEach(toggle => {
+            const moreBtn = toggle.querySelector('.more');
+            moreBtn.addEventListener('click', function () {
+                const additionalContent = toggle.querySelector('.additional-content');
+                if (additionalContent.style.display === "none" || additionalContent.style.display === "") {
+                    additionalContent.style.display = 'block';
+                    moreBtn.textContent = "Read less";
+                } else {
+                    additionalContent.style.display = 'none';
+                    moreBtn.textContent = "Read more";
+                }
+            });
         });
-    });
+    }
 });
