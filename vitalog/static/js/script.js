2f05bc0227136f2c976d65ce12a67c4ffcec086e
gozygreg <goziennabuife@yahoo.com>
2024-04-09 12:57:58 +0000

    add javascript to FAQ page

--- vitalog/static/js/script.js
+++ vitalog/static/js/script.js
@@ -1,34 +1,16 @@
-function toggleText(id) {
-    var elem = document.getElementById(id);
-    var moreLink = elem.querySelector(".more");
-    var additionalContent = elem.querySelector(".additional-content");
-
-    // Check if the additional content is currently hidden
-    if (additionalContent.style.display === "none" || additionalContent.style.display === "") {
-        // Show additional content
-        additionalContent.style.display = 'block';
-        // Update the link text
-        moreLink.textContent = "Read less";
-    } else {
-        // Hide additional content
-        additionalContent.style.display = 'none';
-        // Update the link text
-        moreLink.textContent = "Read more";
-    }
-}
-
-// Function to limit the number of words displayed initially
-function limitWords(element, limit) {
-    var text = element.textContent.trim(); // Trim to remove leading/trailing whitespace
-    var words = text.split(' ');
-    if (words.length > limit) {
-        var truncatedText = words.slice(0, limit).join(' ') + '...';
-        element.innerHTML = truncatedText + ' <span class="more" onclick="toggleText(this.parentElement.parentElement.id)">Read more</span>';
-    }
-}
-
-// Limit the words for each FAQ answer
-var faqElements = document.querySelectorAll('.text-myBlack p');
-faqElements.forEach(function (element) {
-    limitWords(element, 25); // Change 25 to desired word limit
+document.addEventListener("DOMContentLoaded", function () {
+    const toggles = document.querySelectorAll('.toggleText');
+    toggles.forEach(toggle => {
+        const moreBtn = toggle.querySelector('.more');
+        moreBtn.addEventListener('click', function () {
+            const additionalContent = toggle.querySelector('.additional-content');
+            if (additionalContent.style.display === "none" || additionalContent.style.display === "") {
+                additionalContent.style.display = 'block';
+                moreBtn.textContent = "Read less";
+            } else {
+                additionalContent.style.display = 'none';
+                moreBtn.textContent = "Read more";
+            }
+        });
+    });
 });
