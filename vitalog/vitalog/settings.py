3b90b9fabf42c2c92860e7266435e06fe499dda6
gozygreg <goziennabuife@yahoo.com>
2024-04-28 18:01:15 +0000

    postgress

--- vitalog/vitalog/settings.py
+++ vitalog/vitalog/settings.py
@@ -113,12 +113,12 @@ DATABASES = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 
 # Password validation
